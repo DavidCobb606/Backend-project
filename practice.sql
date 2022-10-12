277857f4cec7432e738c7f38d5e40a230b71ff3f
DavidCobb606 <Davidcobb606@gmail.com>
2022-10-12 15:10:13 +0100

    more

--- practice.sql
+++ practice.sql
@@ -2,13 +2,13 @@
 
    
 
-    SELECT articles.article_id, articles.author, articles.body, articles.title, articles.topic, articles.votes, articles.created_at, (COUNT(comments.article_id)) AS comment_count
-    FROM articles    
+      SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body
+    FROM articles
     LEFT JOIN comments
         ON articles.article_id = comments.article_id
-       GROUP BY articles.article_id
-       ORDER BY created_at DESC;
-   WHERE articles.topic = "cats"
+    WHERE articles.article_id = 3
+    ORDER BY created_at DESC;
 
-   
+
+
     
