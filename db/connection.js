df027a8862a21aedaa8cf0b43a40927e234ebdd0
DavidCobb606 <Davidcobb606@gmail.com>
2022-12-16 10:18:53 +0000

    more

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 
+
 const config = 
 ENV === "production"
 ?{ 
@@ -22,7 +23,8 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
  
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
-console.log(process.env.PGDATABASE)
+
 console.log(process.env.DATABASE_URL)
+console.log(process.env.PGDATABASE)
 
 module.exports = new Pool(config);
